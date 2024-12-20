516062d69101ac6ed68fd9f5d87833d3a156207b
bb-grizzle <firbigi1993@gmail.com>
2024-12-20 20:36:47 +0900

    UPDATE | support page

---
+++ src/pages/support.tsx
@@ -0,0 +1,19 @@
+const support = () => {
+	return (
+		<>
+			<h2>우리가 사용하고 싶은</h2>
+			<p>
+				서비스를 만들어요! 우리가 사용하고 싶은 서비스를 만들고 있는 곰과 부엉이입니다. 삶의 풍요를 위해 작게나마 도움이 될만 서비스를 이것저것 만들어 보고 있어요. 서비스 제안, 문의, 지원 등은 아래
+				링크로 부탁드려요 😁
+			</p>
+			<p>
+				email :
+				<a target="__blank" href="mailto:beaowl.kr@gmail.com">
+					beaowl.kr@gmail.com
+				</a>
+			</p>
+		</>
+	);
+};
+
+export default support;
